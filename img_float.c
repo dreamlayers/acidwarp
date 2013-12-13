0aa4499fa421d08772570f0af37b26bfb14a02ec
Boris Gjenero <boris.gjenero@gmail.com>
2013-12-13 12:23:17 -0500

    ANGLE_UNIT should be M_PI*2 not M_PI.
    This fixes appearance of many screens in floating point mode.

--- img_float.c
+++ img_float.c
@@ -9,33 +9,36 @@
 #define mod(x, y) fmod((x), (y))
 #define xor(x, y) ((int)(x) ^ (int)(y))
 
-/* Resulting colours need to go from 0 to 254, with
- * each getting an equal slice of the circle.
+/* ANGLE_UNIT corresponds to M_PI * 2, the full circle in radians.
+ * To avoid visible seams, resulting colours need to go from 0 to 254,
+ * with each getting an equal slice of the circle.
  */
 #define ANGLE_UNIT                  (255.0)
-#define TRIG_UNIT                   (ANGLE_UNIT*2)
-/* The same idea as 2*PI, PI/2, PI/4, etc. */
 #define ANGLE_UNIT_2                (ANGLE_UNIT*2)
 #define ANGLE_UNIT_HALF             (ANGLE_UNIT/2)
 #define ANGLE_UNIT_EIGHTH           (ANGLE_UNIT/8)
 #define ANGLE_UNIT_QUART            (ANGLE_UNIT/4)
 #define ANGLE_UNIT_THREE_QUARTERS   (ANGLE_UNIT*3/4)
 
+/* TRIG_UNIT is full scale of trig functions. It was changed to
+ * try to avoid visible seams where the palette wraps.
+ */
+#define TRIG_UNIT                   (ANGLE_UNIT*2)
 
 static double lut_sin (double a)
 {
-    return TRIG_UNIT * sin(a * M_PI / ANGLE_UNIT);
+    return TRIG_UNIT * sin(a * M_PI * 2 / ANGLE_UNIT);
 }
 
 
 static double lut_cos (double a)
 {
-    return TRIG_UNIT * cos(a * M_PI / ANGLE_UNIT);
+    return TRIG_UNIT * cos(a * M_PI * 2 / ANGLE_UNIT);
 }
 
 static double lut_angle (double dx, double dy)
 {
-    double angle = atan2(dy, dx) * ANGLE_UNIT / M_PI;
+    double angle = atan2(dy, dx) * ANGLE_UNIT / (M_PI * 2);
     /* Always return a positive result */
     if (angle < 0.0) angle += (double)ANGLE_UNIT_2;
     return angle;
