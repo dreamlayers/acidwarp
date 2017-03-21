dreamlayers/acidwarp

commit 70e2cc43826e580fbe4e55e56a44ae00c4631e60

Fix for uname -o breaking in MacOS

Makefile
@@ -2,7 +2,8 @@ CFLAGS := -g -O2 -Wall -Wmissing-prototypes
 SOURCES := acidwarp.c bit_map.c lut.c palinit.c rolnfade.c display.c img_float.c acid_ico.c
 OBJECTS := $(SOURCES:%.c=%.o)
 
-PLATFORM := $(shell uname -o)
+PLATFORM := $(shell uname)
+
 ifeq ($(PLATFORM),Emscripten)
 CC = emcc
 EXESUFFIX = .html

commit 507507c3e53997cf50c32f4946f6e12379451b4b

Fix Cygwin detection in Makefile, broken in 70e2cc4
Cygwin uname -o return Cygwin, but uname returns CYGWIN_NT-6.1
or similar.

Makefile
@@ -45,7 +45,7 @@ LIBS += -lGL
 CFLAGS += -DWITH_GL
 endif
 
-ifeq ($(PLATFORM),Cygwin)
+ifneq (,$(findstring CYGWIN,$(PLATFORM)))
 EXESUFFIX = .exe
 CC := i686-w64-mingw32-gcc
 OBJECTS += acid_res.o
@@ -69,7 +69,7 @@ palinit.o: palinit.c handy.h acidwarp.h palinit.h
 rolnfade.o: rolnfade.c handy.h acidwarp.h rolnfade.h palinit.h display.h
 display.o: display.c display.h acidwarp.h handy.h
 img_float.o: img_float.c gen_img.c acidwarp.h
-ifeq ($(PLATFORM),Cygwin)
+ifneq (,$(findstring CYGWIN,$(PLATFORM)))
 acidwarp.ico: acidwarp.png
 	icotool -c -o $@ $^
 acid_res.o: acid_res.rc acidwarp.ico